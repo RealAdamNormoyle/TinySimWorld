256f18c5642bfd901cf5e8c462e51595466fb6f8
NormoyleAdam <contact@adamnormoyle.co.uk>
2021-02-02 23:35:00 +0000

    Added simple character script

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class Character : MonoBehaviour
+{
+    public bool IsMoving { get { return m_lookDirection != Vector2.zero; } }
+    [SerializeField] CharacterPartCollection[] m_partCollections;
+    
+    Animator m_anim;
+    Vector2 m_lookDirection = Vector2.zero;
+
+    void Start()
+    {
+        m_anim = GetComponent<Animator>();
+        HideAllParts();
+        m_partCollections[0].gameObject.SetActive(true);
+
+    }
+
+    public void UpdateLookDirection(Vector2 dir)
+    {
+        m_lookDirection = dir;
+
+        if (IsMoving)
+        {
+            Debug.Log(dir);
+            HideAllParts();
+            if(dir.y > 0)
+            {
+                m_partCollections[2].gameObject.SetActive(true);
+            }else if(dir.y < 0)
+            {
+                m_partCollections[0].gameObject.SetActive(true);
+            }
+            else
+            {
+                m_partCollections[1].gameObject.SetActive(true);
+                transform.localScale = new Vector3((m_lookDirection.x < 0) ? 1 : -1, 1, 1);
+            }
+        }
+    }
+
+    void HideAllParts()
+    {
+        m_partCollections[0].gameObject.SetActive(false);
+        m_partCollections[1].gameObject.SetActive(false);
+        m_partCollections[2].gameObject.SetActive(false);
+    }
+
+    void FixedUpdate()
+    {
+        m_anim.SetBool("Walking", IsMoving);
+    }
+}
---
+++ Assets/Scripts/CharacterPartCollection.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterPartCollection : MonoBehaviour
+{
+    [SerializeField] SpriteRenderer m_face;
+    [SerializeField] SpriteRenderer m_head;
+    [SerializeField] SpriteRenderer m_body;
+    [SerializeField] SpriteRenderer m_leftShoe;
+    [SerializeField] SpriteRenderer m_rightShoe;
+
+    public void SetHeadSprite(Sprite sprite) { m_head.sprite = sprite; }
+    public void SetBodySprite(Sprite sprite) { m_body.sprite = sprite; }
+    public void SetShoeSprite(Sprite sprite) { m_leftShoe.sprite = m_rightShoe.sprite = sprite; }
+
+}
+
+[System.Serializable]
+public class CharacterSpriteCollection
+{
+    public Sprite Front;
+    public Sprite Back;
+    public Sprite Side;
+}
