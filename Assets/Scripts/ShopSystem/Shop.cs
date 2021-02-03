1536ac51f44cba18775fd693660c2501243c162a
NormoyleAdam <contact@adamnormoyle.co.uk>
2021-02-03 23:44:22 +0000

    Load up shop items from resources

--- Assets/Scripts/ShopSystem/Shop.cs
+++ Assets/Scripts/ShopSystem/Shop.cs
@@ -13,12 +13,24 @@ public class Shop : MonoBehaviour
     [SerializeField] string m_name;
     [SerializeField] ShopType m_shopType;
 
+    List<Item> m_items = new List<Item>();
     public string ShopName { get { return m_name; } }
     public ShopType Type { get { return m_shopType; } }
+    public List<Item> Items { get { return m_items; } }
 
     // Start is called before the first frame update
     void Start()
     {
+        LoadItems();
+    }
+
+    void LoadItems()
+    {
+        Item[] tempItems = Resources.LoadAll<Item>(string.Concat("Items/", m_shopType.ToString()));
+
+        foreach (var item in tempItems)
+            if(item.Active)
+                m_items.Add(item);
         
     }
 
