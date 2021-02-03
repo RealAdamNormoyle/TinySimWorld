2ed5718fa59979e3908709e05cf29bc3798b34fd
NormoyleAdam <contact@adamnormoyle.co.uk>
2021-02-03 21:39:23 +0000

    Added boilerplate singleton

---
+++ Assets/Scripts/Utilities/Singleton.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : Component
+{
+    private static T m_instance;
+    [SerializeField] 
+    private bool m_dontDestroyOnLoad = true;
+
+    public static T Instance
+    {
+        get
+        {
+            if (m_instance == null)
+            {
+                m_instance = FindObjectOfType<T>();
+
+                if (m_instance == null)
+                {
+                    var go = new GameObject();
+                    go.hideFlags = HideFlags.DontSave;
+                    go.name = typeof(T).Name + " (Singleton)";
+                    m_instance = go.AddComponent<T>();
+                }
+            }
+
+            return m_instance;
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        if (m_dontDestroyOnLoad)
+            DontDestroyOnLoad(this);
+
+        if (m_instance == null)
+            m_instance = this as T;
+        else
+            Destroy(gameObject);
+    }
+
+}
