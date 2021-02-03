20e5cb7c20001f879b42dc056d0f039652918a72
NormoyleAdam <contact@adamnormoyle.co.uk>
2021-02-03 23:42:22 +0000

    Added UI Screen base class

---
+++ Assets/Scripts/Utilities/UIScreen.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+[RequireComponent(typeof(CanvasGroup))]
+public class UIScreen : MonoBehaviour
+{
+    CanvasGroup m_canvas;
+
+    public virtual void Awake()
+    {
+        m_canvas = GetComponent<CanvasGroup>();
+    }
+
+    public virtual void Show()
+    {
+        m_canvas.alpha = 1;
+        m_canvas.blocksRaycasts = true;
+        m_canvas.interactable = true;
+    }
+
+    public virtual void Hide()
+    {
+        m_canvas.alpha = 0;
+        m_canvas.blocksRaycasts = false;
+        m_canvas.interactable = false;
+    }
+
+
+}
