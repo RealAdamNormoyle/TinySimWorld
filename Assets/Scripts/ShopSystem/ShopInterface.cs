93d8e38d1c9088651e96ef71e29ae95b1a1ee23b
NormoyleAdam <contact@adamnormoyle.co.uk>
2021-02-03 23:44:53 +0000

    Finalized core shop interface

--- Assets/Scripts/ShopSystem/ShopInterface.cs
+++ Assets/Scripts/ShopSystem/ShopInterface.cs
@@ -4,43 +4,83 @@ using UnityEngine;
 using UnityEngine.UI;
 
 [RequireComponent(typeof(CanvasGroup))]
-public class ShopInterface : MonoBehaviour
+public class ShopInterface : UIScreen
 {
     [SerializeField] Text m_titleText;
     [SerializeField] Text m_coinsText;
     [SerializeField] Text m_gemsText;
     [SerializeField] Transform m_storeContentContainer;
+    [SerializeField] GameObject m_itemPrefab;
 
-    CanvasGroup m_canvas;
     Shop m_activeShop;
+    List<ShopItem> m_items = new List<ShopItem>();
 
     public void Show(Shop shop)
     {
-        m_canvas.alpha = 1;
-        m_canvas.blocksRaycasts = true;
-        m_canvas.interactable = true;
+        base.Show();
+        m_activeShop = shop;
+        UpdateStoreDetails();
+        UpdateStoreContent();
 
         GameManager.Instance.Player?.AllowInput(false);
-
-        m_activeShop = shop;
     }
 
-    public void Hide()
+    public override void Hide()
     {
-        m_canvas.alpha = 0;
-        m_canvas.blocksRaycasts = false;
-        m_canvas.interactable = false;
-
+        base.Hide();
         GameManager.Instance.Player?.AllowInput(true);
     }
 
     void UpdateStoreDetails()
     {
-
+        m_titleText.text = m_activeShop.ShopName;
+        m_coinsText.text = GameManager.Instance.PlayerInventory.Coins.ToString();
+        m_gemsText.text = GameManager.Instance.PlayerInventory.Gems.ToString();
     }
 
     void UpdateStoreContent()
     {
+        for (int i = 0; i < m_items.Count;)
+        {
+            Destroy(m_items[0].gameObject);
+            m_items.RemoveAt(0);
+        }
 
+        foreach (var item in m_activeShop.Items)
+        {
+            ShopItem shopItem = GameObject.Instantiate(m_itemPrefab,m_storeContentContainer).GetComponent<ShopItem>();
+            shopItem.Setup(item,BuyItem,SellItem);
+        }
+
+    }
+
+    void UpdateItemButtons()
+    {
+        for (int i = 0; i < m_items.Count;i++)
+        {
+            m_items[i].RefreshButtons();
+        }
+    }
+
+    public void BuyItem(Item item)
+    {
+        if (GameManager.Instance.PlayerInventory.Coins < item.Cost)
+            return;
+
+        GameManager.Instance.PlayerInventory.Coins -= item.Cost;
+        GameManager.Instance.PlayerInventory.Items.Add(item);
+
+        UpdateItemButtons();
+    }
+
+    public void SellItem(Item item)
+    {
+        if (!GameManager.Instance.PlayerInventory.Items.Contains(item))
+            return;
+
+        if(GameManager.Instance.PlayerInventory.Items.Remove(item))
+            GameManager.Instance.PlayerInventory.Coins += item.Cost / 2;
+
+        UpdateItemButtons();
     }
 }
