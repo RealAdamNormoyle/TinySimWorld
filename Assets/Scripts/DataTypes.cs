7a50dfebe2b3d5da2dad62db3158b30e748bc5c5
NormoyleAdam <contact@adamnormoyle.co.uk>
2021-02-03 23:41:56 +0000

    Added data types class

---
+++ Assets/Scripts/DataTypes.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerInventory
+{
+    public int Coins;
+    public int Gems;
+    public List<Item> Items = new List<Item>();
+}
