e5d069c462c036be6fbd50a8052b3306ffd6f107
NormoyleAdam <contact@adamnormoyle.co.uk>
2021-02-03 23:43:33 +0000

    Added inventory Interface script

---
+++ Assets/Scripts/Inventory/InventoryInterface.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryInterface : UIScreen
+{
+    [SerializeField] Transform m_contentContainer;
+    [SerializeField] GameObject m_itemPrefab;
+    List<ShopItem> m_items = new List<ShopItem>();
+
+
+    public override void Show()
+    {
+        base.Show();
+        GameManager.Instance.Player?.AllowInput(false);
+    }
+
+    public override void Hide()
+    {
+        base.Hide();
+        GameManager.Instance.Player?.AllowInput(true);
+    }
+
+
+}
