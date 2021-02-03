c273afe78033c5b1454f58ce7e846e132d88f00d
NormoyleAdam <contact@adamnormoyle.co.uk>
2021-02-03 21:41:26 +0000

    Basic GameManager for now

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+    [SerializeField] ShopInterface m_shopInterface;
+    [SerializeField] GameObject m_mainMenu;
+
+    GameObject m_playerPrefab;
+    PlayerController m_player;
+    
+    public PlayerController Player { get { return m_player; } }
+    public ShopInterface ShopInterface { get { return m_shopInterface; } }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_playerPrefab = Resources.Load<GameObject>("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnPlayer()
+    {
+        if(m_player)
+            return;
+
+
+        m_player = GameObject.Instantiate(m_playerPrefab).GetComponent<PlayerController>();
+    }
+}
