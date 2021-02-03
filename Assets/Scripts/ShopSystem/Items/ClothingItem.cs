c1d4a441b10326957ffe9217944f41ca37c45a57
NormoyleAdam <contact@adamnormoyle.co.uk>
2021-02-03 23:43:14 +0000

    Added clothing item scriptable object class

---
+++ Assets/Scripts/ShopSystem/Items/ClothingItem.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewClothingItem", menuName = "Shop/Items/ClothingItem", order = 0)]
+public class ClothingItem : Item
+{
+    public enum SpriteDirection
+    {
+        Up,
+        Down,
+        Side
+    }
+
+    [SerializeField] Sprite[] m_sprites;
+    public Sprite[] Sprites { get {return m_sprites; } }
+}
