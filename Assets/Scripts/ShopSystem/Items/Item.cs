f8b4408c202ed17d4ea1f35ec4a10349579bc49c
NormoyleAdam <contact@adamnormoyle.co.uk>
2021-02-03 23:42:57 +0000

    Added shop item base class

---
+++ Assets/Scripts/ShopSystem/Items/Item.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : ScriptableObject
+{
+    [SerializeField] bool m_active;
+    [SerializeField] string m_name;
+    [SerializeField] string m_description;
+    [SerializeField] Sprite m_icon;
+    [SerializeField] int m_cost;
+
+    public bool Active { get { return m_active; } }
+    public string Name { get { return m_name; } }
+    public Sprite Icon { get { return m_icon; } }
+    public int Cost { get { return m_cost; } }
+    public string Description { get { return m_description; } }
+}
