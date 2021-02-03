fde66b76baa2ace33b07f8e388b773512dcf90ad
NormoyleAdam <contact@adamnormoyle.co.uk>
2021-02-03 21:41:52 +0000

    Starting point for shop object script

---
+++ Assets/Scripts/ShopSystem/Shop.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    public enum ShopType
+    {
+        Clothing
+    }
+
+
+    [SerializeField] string m_name;
+    [SerializeField] ShopType m_shopType;
+
+    public string ShopName { get { return m_name; } }
+    public ShopType Type { get { return m_shopType; } }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void ShowInterface()
+    {
+        GameManager.Instance.ShopInterface.Show(this);
+    }
+}
