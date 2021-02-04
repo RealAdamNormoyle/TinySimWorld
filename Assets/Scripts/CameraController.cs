748e8ad46ed19efa1eeee682315c104e03a44f56
NormoyleAdam <contact@adamnormoyle.co.uk>
2021-02-04 10:51:16 +0000

    Added simple camera controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] Transform m_target;
+
+
+    public void SetTarget(Transform target)
+    {
+        m_target = target;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (m_target == null)
+            return;
+
+        transform.position = Vector2.Lerp(transform.position, m_target.position,10*Time.deltaTime);
+    }
+}
