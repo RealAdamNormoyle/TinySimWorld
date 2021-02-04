eb7e7ea813272ad72ae1e9d7659186b7381dc8ee
NormoyleAdam <contact@adamnormoyle.co.uk>
2021-02-04 10:58:21 +0000

    Updated item script - Added item type

--- Assets/Scripts/ShopSystem/Items/Item.cs
+++ Assets/Scripts/ShopSystem/Items/Item.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Item : ScriptableObject
 {
+    public enum ItemType
+    {
+        CLOTHING_TOP,
+        CLOTHING_SHOES
+    }
+
     [SerializeField] bool m_active;
     [SerializeField] string m_name;
     [SerializeField] string m_description;
     [SerializeField] Sprite m_icon;
     [SerializeField] int m_cost;
+    [SerializeField] ItemType m_type;
 
     public bool Active { get { return m_active; } }
     public string Name { get { return m_name; } }
     public Sprite Icon { get { return m_icon; } }
     public int Cost { get { return m_cost; } }
     public string Description { get { return m_description; } }
+    public ItemType Type { get { return m_type; } }
 }
