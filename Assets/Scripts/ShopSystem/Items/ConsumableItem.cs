2027bdf3ecd70ce6e2aafcca1adf856e19a2139d
NormoyleAdam <contact@adamnormoyle.co.uk>
2021-02-04 17:48:25 +0000

    Added empty consumable class - this would be filled with consumable related code

---
+++ Assets/Scripts/ShopSystem/Items/ConsumableItem.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewConsumableItem", menuName = "Shop/Items/ConsumableItem", order = 0)]
+public class ConsumableItem : Item
+{
+
+
+}
