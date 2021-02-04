249c11fa5cac7d97a3ef6157fb3d635c4b1ab576
NormoyleAdam <contact@adamnormoyle.co.uk>
2021-02-04 10:59:53 +0000

    Setup player & camera spawning Added start game function Added Player inventory & Main menu reference

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,34 +5,54 @@ using UnityEngine;
 public class GameManager : Singleton<GameManager>
 {
     [SerializeField] ShopInterface m_shopInterface;
+    [SerializeField] InventoryInterface m_inventoryInterface;
     [SerializeField] GameObject m_mainMenu;
+    //[SerializeField] GameObject m_mainMenu;
 
     GameObject m_playerPrefab;
+    GameObject m_cameraPrefab;
     PlayerController m_player;
+    CameraController m_camera;
 
     public PlayerInventory PlayerInventory = new PlayerInventory();
     public PlayerController Player { get { return m_player; } }
     public ShopInterface ShopInterface { get { return m_shopInterface; } }
-
+    public InventoryInterface InventoryInterface { get { return m_inventoryInterface; } }
 
     // Start is called before the first frame update
     void Start()
     {
         m_playerPrefab = Resources.Load<GameObject>("Player");
+        m_cameraPrefab = Resources.Load<GameObject>("Camera");
+
+        m_mainMenu.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StartGame()
     {
-        
+        m_mainMenu.SetActive(false);
+        SpawnPlayerAndCamera();
     }
 
-    void SpawnPlayer()
+    void SpawnPlayerAndCamera()
     {
         if(m_player)
             return;
 
-
         m_player = GameObject.Instantiate(m_playerPrefab).GetComponent<PlayerController>();
+        m_camera = GameObject.Instantiate(m_cameraPrefab).GetComponent<CameraController>();
+        m_camera.SetTarget(m_player.transform);
+    }
+
+    public void PlayerEquipItem(Item item)
+    {
+        if((int)item.Type < 2)
+        {
+            PlayerInventory.Items.Remove(item);
+            PlayerInventory.Items.Add(PlayerInventory.EquippedItems[(int)item.Type]);
+            PlayerInventory.EquippedItems[(int)item.Type] = item;
+
+            Player.EquipClothingItem((ClothingItem)item);
+        }
     }
 }
